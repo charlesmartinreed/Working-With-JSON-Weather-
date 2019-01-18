e32b7e0d3e8fa737235693d2927e0a07fb918903
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-18 16:55:09 -0600

    Implement delegate methods for collection view and flow layout. Set scrolling direction to horizontal.

--- Working With JSON (Dark Sky)/Working With JSON (Dark Sky)/AppDelegate.swift
+++ Working With JSON (Dark Sky)/Working With JSON (Dark Sky)/AppDelegate.swift
@@ -24,7 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        randomViewController.view.backgroundColor = .purple
         
         //MARK:- building UICollectionViewCells
-        let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewFlowLayout() 
+        layout.scrollDirection = .horizontal
+        
         let forecastFrameController = ForecastFrameController(collectionViewLayout: layout)
         window?.rootViewController = forecastFrameController
         
